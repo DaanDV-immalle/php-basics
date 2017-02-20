b1d72ddb7e10801a07debb9b913bd045d31c053d
Daan DV <daan.devry@immalle.be>
2017-02-20 09:49:12 +0100

    Oef 5

--- 04_constants.php
+++ 04_constants.php
@@ -3,7 +3,12 @@
 define("USERNAME", "joske");
 
 echo USERNAME;
-
+echo '<br>';
 // definieer ook een constante PASSWORD en SERVERNAME
+define("PASSWORD", "Test123");
+define("SERVERNAME", "Google XPQ5000");
 
+echo PASSWORD;
+echo '<br>';
+echo SERVERNAME;
 ?>
